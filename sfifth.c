a2512262587555b617286b6b0d51158b2f85eb56
arcissist <1164801452@qq.com>
2024-08-01 17:09:39 +0800

    Correction for sfifth.c, using the memset function in the source code.

--- sfifth.c
+++ sfifth.c
@@ -6,6 +6,7 @@ int main( int argc, const char** argv )
 	char address[100] = "";
 	char findChar = 0;
 	int i = 0, tab = 0;
+	memset( address, 0, sizeof(address) );
 	printf( "Please enter a character sting:" );
 	fgets( address, 100, stdin );
 	printf( "Please enter a letter:" );
